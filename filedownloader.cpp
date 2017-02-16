6a7c7c1fcfffbab41996988ab56ce6ea3a98be26
Koala BR <34948369+KoalaBR@users.noreply.github.com>
2017-02-16 21:31:28 +0100

    First version, download hardcoded url and splits it up.

---
+++ filedownloader.cpp
@@ -0,0 +1,6 @@
+#include "filedownloader.h"
+
+FileDownloader::FileDownloader()
+{
+
+}
---
+++ imageitem.h
@@ -0,0 +1,11 @@
+#ifndef IMAGEITEM_H
+#define IMAGEITEM_H
+
+
+class ImageItem
+{
+public:
+    ImageItem();
+};
+
+#endif // IMAGEITEM_H
