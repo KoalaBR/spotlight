6c67930ddcc3875f3ac0186a48dd65ab1ed43130
Koala BR <34948369+KoalaBR@users.noreply.github.com>
2018-01-13 22:34:28 +0100

    Implemented missing functionality for "Set title"

--- dialogrename.cpp
+++ dialogrename.cpp
@@ -1,14 +1,91 @@
+#include <QFile>
+#include <QMessageBox>
+#include <QDebug>
+
 #include "dialogrename.h"
 #include "ui_dialogrename.h"
 
-DialogRename::DialogRename(QWidget *parent) :
+DialogRename::DialogRename(const ImageItem item, Database *db, QString baseDir, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::DialogRename)
 {
+    m_db   = db;
+    m_item = item;
+    m_baseDir = baseDir;
     ui->setupUi(this);
+    ui->leTitle->setText(item.title());
+    connect(ui->leTitle,  SIGNAL(textChanged(QString)), this, SLOT(slotTitleChanged(QString)));
+    connect(ui->pbRename, SIGNAL(clicked(bool)),        this, SLOT(slotRenameClicked()));
 }
 
 DialogRename::~DialogRename()
 {
     delete ui;
+}
+
+QString DialogRename::getNewTitle(void)
+{
+    return ui->leTitle->text();
+}
+
+bool DialogRename::isTitleUsed(QString title)
+{
+    ImageItem newItem = m_item;
+    newItem.setTitle(title);
+    QString filename = m_baseDir + newItem.filename();
+    QFile test(filename);
+    return test.exists();
+}
+
+void DialogRename::slotTitleChanged(QString title)
+{
+    QPalette *palette = new QPalette();
+    if (isTitleUsed(title.trimmed()))
+    {
+        palette->setColor(QPalette::Text,Qt::red);
+        ui->pbRename->setEnabled(false);
+    }
+    else
+    {
+        palette->setColor(QPalette::Text, Qt::black);
+        ui->pbRename->setEnabled(true);
+    }
+    ui->leTitle->setPalette(*palette);
+}
+
+void DialogRename::slotRenameClicked(void)
+{
+    QString title = ui->leTitle->text().trimmed();
+    if (title.length() == 0)
+        return;
+    ImageItem newItem = m_item;
+    newItem.setTitle(title);
+    QString newFilename = m_baseDir + newItem.filename();
+    QString oldFilename = m_baseDir + m_item.filename();
+    qDebug() << oldFilename;
+    QFile file(oldFilename);
+    if (file.rename(newFilename))
+    {
+        if (m_db->setNewTitle(m_item, title))
+        {
+            accept();
+        }
+        else
+        {
+            // Undo rename, because we can't update the database
+            QFile fileUndo(newFilename);
+            if (!fileUndo.rename(oldFilename))
+            {
+                QMessageBox::critical(this, tr("Rename failed (Undo)"),
+                                      tr("Database update failed but couldn't rename file back!?") +"\n" + fileUndo.errorString(),
+                                      QMessageBox::Ok);
+            }
+        }
+    }
+    else
+    {
+        // Rename failed, just alert the user
+        QMessageBox::critical(this, tr("Rename failed"),
+                              tr("Rename failed. System says:") +"\n" + file.errorString(), QMessageBox::Ok);
+    }
 }
--- dialogrename.h
+++ dialogrename.h
@@ -1,6 +1,9 @@
 #ifndef DIALOGRENAME_H
 #define DIALOGRENAME_H
 
+#include "imageitem.h"
+#include "database.h"
+
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +15,20 @@ class DialogRename : public QDialog
     Q_OBJECT
 
 public:
-    explicit DialogRename(QWidget *parent = 0);
+    explicit DialogRename(const ImageItem item, Database *db,
+                          QString baseDir, QWidget *parent = 0);
     ~DialogRename();
-
+    QString         getNewTitle(void);
+public slots:
+    void        slotTitleChanged(QString title);                    ///< Called once the text in the edit field changed
+    void        slotRenameClicked();                                ///< User requested rename
 private:
+    bool        isTitleUsed(QString title);   ///< is there a filename collision with the requested title?
+
     Ui::DialogRename *ui;
+    ImageItem       m_item;
+    Database       *m_db;
+    QString         m_baseDir;
 };
 
 #endif // DIALOGRENAME_H
